56f53a9391aa755a85c49aa4dd9ddc1711fb17f0
dar-gol <dariusz.w.golom@gmail.com>
2022-06-02 17:26:58 +0200

    add edit email and change password and fix add category

--- src/components/SimpleInput/TextInput.tsx
+++ src/components/SimpleInput/TextInput.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { useFormContext } from 'react-hook-form';
-import { IProps } from '../../@types/inputs';
-import { Col, InputText } from '../../styles/global.styled';
+import React from "react";
+import { useFormContext } from "react-hook-form";
+import { IProps } from "../../@types/inputs";
+import { Col, InputText } from "../../styles/global.styled";
 
-const TextInput = ({ name, title, required }: IProps) => {
+const TextInput = ({
+  name,
+  title,
+  required,
+  type,
+}: IProps & { type?: string }) => {
   const { register } = useFormContext();
   return (
     <Col>
       <p>{title}:</p>
-      <InputText {...register(name, { required })} />
+      <InputText {...register(name, { required })} type={type} />
     </Col>
   );
 };
+
+TextInput.defaultProps = { type: "text" };
 
 export default TextInput;
