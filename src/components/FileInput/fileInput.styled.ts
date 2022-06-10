ce8f00eb30aa16c8e0c2fc30e29728a6ad17d9f1
dar-gol <dariusz.w.golom@gmail.com>
2022-06-10 20:23:28 +0200

    create drag and drop image uploader

---
+++ src/components/FileInput/fileInput.styled.ts
@@ -0,0 +1,74 @@
+import { FileUploader } from 'react-drag-drop-files';
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const Preview = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+  background-color: white;
+  border: 2px dashed #f47521;
+  border-radius: 10px;
+`;
+
+export const ImgPreview = styled.img`
+  width: 100%;
+`;
+
+export const Title = styled.p`
+  font-size: 14px;
+`;
+
+export const FileStyle = createGlobalStyle`
+  .file-uploader {
+    min-width: unset;
+    max-width: unset;
+    height: 400px;
+    flex-grow: 1;
+    border-color: #F47521;
+    flex-direction: column;
+    justify-content: center;
+    gap: 30px;
+  }
+  .file-uploader svg {
+    transform: scale(2);
+  }
+
+  .file-uploader svg path {
+    fill: #F47521;
+  }
+
+  .file-uploader > div {
+    gap: 10px;
+    flex-grow: 0;
+    flex-direction: column;
+  }
+
+  .file-uploader > div > span {
+    text-align: center;
+    max-width: unset !important;
+  }
+
+  .file-uploader > div > span.file-types {
+    font-weight: 600;
+  }
+`;
+
+export const BtnDelete = styled.button`
+  font-weight: 900;
+  font-size: 20px;
+  position: absolute;
+  top: -10px;
+  right: -8px;
+  height: 35px;
+  width: 35px;
+  border: 0;
+  border-radius: 100%;
+  background-color: #d75252;
+  color: white;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  display: ${({ hide }: { hide?: boolean }) => (hide ? 'none' : 'block')};
+`;
--- src/components/FileInput/fileInput.tsx
+++ src/components/FileInput/fileInput.tsx
@@ -1,14 +1,69 @@
-import React from 'react';
-import { useFormContext } from 'react-hook-form';
+import React, { useState } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+import { FileUploader } from 'react-drag-drop-files';
+import { Trash2 } from 'react-feather';
 import { IProps } from '../../@types/inputs';
+import { Col, Row } from '../../styles/global.styled';
+import {
+  Preview,
+  FileStyle,
+  BtnDelete,
+  ImgPreview,
+  Title,
+} from './fileInput.styled';
 
-const FileInput = ({ name, title, required }: IProps) => {
-  const { register } = useFormContext();
+const FileInput = ({
+  name,
+  title,
+  required,
+  remove,
+}: IProps & { remove: (type: string) => void }) => {
+  const { control } = useFormContext();
+  const [img, setImg] = useState<string>('');
+
+  const onImageChange = (file: any) => {
+    setImg(URL.createObjectURL(file));
+  };
+
+  const handleRemove = async () => {
+    try {
+      await remove(name);
+      setImg('');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
-    <div>
-      <p>{title}</p>
-      <input {...register(name, { required })} type="file" />
-    </div>
+    <Row justifyContent="center" margin="20px 0" flex="1" position="relative">
+      <FileStyle />
+      <Controller
+        name={name}
+        control={control}
+        rules={{ required }}
+        render={({ field }) => (
+          <FileUploader
+            {...field}
+            name="file"
+            types={['PNG']}
+            classes="file-uploader"
+            label={title}
+            handleChange={(file: any) => {
+              onImageChange(file);
+              field.onChange(file);
+            }}
+          />
+        )}
+      />
+      {img && (
+        <Preview>
+          <BtnDelete type="button" onClick={() => handleRemove()}>
+            <Trash2 size={18} />
+          </BtnDelete>
+          <ImgPreview src={img} alt="This introduce choosen alcohol" />
+        </Preview>
+      )}
+    </Row>
   );
 };
 
