77e69de96462b975cb7b41713737550143bade2c
dar-gol <dariusz.w.golom@gmail.com>
2022-10-01 21:35:51 +0200

    change file input

--- src/components/FileInput/fileInput.tsx
+++ src/components/FileInput/fileInput.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Controller, useFormContext } from 'react-hook-form';
-import { FileUploader } from 'react-drag-drop-files';
-import { Trash2 } from 'react-feather';
-import { IProps } from '../../@types/inputs';
-import { Col, Row } from '../../styles/global.styled';
+import React, { useEffect, useState } from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import { FileUploader } from "react-drag-drop-files";
+import { Trash2 } from "react-feather";
+import { IProps } from "../../@types/inputs";
+import { Col, Row } from "../../styles/global.styled";
 import {
   Preview,
   FileStyle,
@@ -11,8 +11,8 @@ import {
   ImgPreview,
   PreviewBlock,
   Title,
-} from './fileInput.styled';
-import { URL as URLS } from '../../utils/constant';
+} from "./fileInput.styled";
+import { URL as URLS } from "../../utils/constant";
 
 const FileInput = ({
   name,
@@ -21,68 +21,70 @@ const FileInput = ({
   remove,
   imageName,
 }: IProps & { remove: (type: string) => void; imageName?: string }) => {
-  const { control, setValue, getValues } = useFormContext();
-  const [img, setImg] = useState<string>(
-    imageName ? `${URLS.GET_IMAGE}/${imageName}` : ''
-  );
+  const { control } = useFormContext();
+  // const [img, setImg] = useState<string>(
+  //   imageName ? `${URLS.GET_IMAGE}/${imageName}` : ""
+  // );
 
-  const onImageChange = (file: any) => {
-    setImg(URL.createObjectURL(file));
-  };
+  // const onImageChange = (file: any) => {
+  //   setImg(URL.createObjectURL(file));
+  // };
 
   const handleRemove = async () => {
     try {
       await remove(name);
-      setImg('');
+      // setImg("");
     } catch (e) {
       console.error(e);
     }
   };
 
-  useEffect(() => {
-    if (!imageName) setImg('');
-  }, [imageName]);
+  // useEffect(() => {
+  //   if (!imageName) setImg("");
+  // }, [imageName]);
 
   return (
-    <Row justifyContent="center" margin="20px 0" flex="1" position="relative">
+    <Row justifyContent="center" flex="1" position="relative">
       <FileStyle />
       <Controller
         name={name}
         control={control}
         rules={{ required }}
         render={({ field }) => (
-          <FileUploader
-            {...field}
-            name="file"
-            types={['PNG']}
-            classes="file-uploader"
-            label={title}
-            handleChange={(file: any) => {
-              onImageChange(file);
-              field.onChange(file);
-            }}
-          />
+          <>
+            <FileUploader
+              key={field.ref}
+              inputRef={field.ref}
+              types={["PNG"]}
+              classes="file-uploader"
+              label={title}
+              handleChange={(file: any) => {
+                field.onChange(file);
+              }}
+              {...field}
+            />
+            {field.value && (
+              <Preview>
+                <BtnDelete type="button" onClick={() => handleRemove()}>
+                  <Trash2 size={18} />
+                </BtnDelete>
+                <PreviewBlock>
+                  <ImgPreview
+                    src={URL.createObjectURL(field.value)}
+                    alt="This introduce choosen alcohol"
+                  />
+                </PreviewBlock>
+              </Preview>
+            )}
+          </>
         )}
       />
-      {img && (
-        <Preview>
-          <BtnDelete type="button" onClick={() => handleRemove()}>
-            <Trash2 size={18} />
-          </BtnDelete>
-          <PreviewBlock>
-            <ImgPreview
-              src={`${img}#t?t=${new Date().getTime()}`}
-              alt="This introduce choosen alcohol"
-            />
-          </PreviewBlock>
-        </Preview>
-      )}
     </Row>
   );
 };
 
 FileInput.defaultProps = {
-  imageName: '',
+  imageName: "",
 };
 
 export default FileInput;
